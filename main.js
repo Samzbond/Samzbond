97ddb23e1d4e601e88141052757830f4e9ec7e92
Samzbond <40712693+Samzbond@users.noreply.github.com>
2022-10-31 23:37:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ var getScriptPromisify = (src) => {
                 max: 100,
                 splitNumber: 10,
                 itemStyle: {
-                    color: '#FFFFFF'
+                    color: '#0000FF'
                 },
                 progress: {
                     show: true,
